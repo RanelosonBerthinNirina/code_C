382fb79d4a094431219bf1d48063b8a6ad8c2f00
RanelosonBerthinNirina <nirinaranel@gmail.com>
2026-02-07 11:14:37 +0300

    terminer bintodecimal

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <unistd.h>
+#include <ctype.h>
+#include <math.h>
 char *smash (const char *const words[/* count */], size_t count){
     char* cible=malloc(sizeof(char));
    if (count<=1){
@@ -30,9 +32,37 @@ char *smash (const char *const words[/* count */], size_t count){
     return phrase;
     free(cible), free(phrase);
 }
+int *reverse_list(const int *array, size_t length) {
+       //  <---  hajime!
+  if (!array)
+    return 0;
+    size_t i=0,z=length-1;
+    int *tableau = malloc(length* sizeof(int));
+    *tableau=0;
+    while (i<=length){
+        tableau[i]=array[z];
+        z--,i++;
+    }
+  return tableau;
+    free(tableau);
+    tableau = NULL;
+}
+unsigned bin_to_decimal(const char *bin){
+    size_t len= strlen(bin), puissance=1,decimal=0;
+    for (size_t i = 0; i <len-1; i++){
+        puissance*=2;
+    }
+    while (*bin){
+        if (*bin=='1'){
+            // printf("1");
+            decimal+=puissance;
+        }
+        *bin++,puissance/=2;
+    }
+	return decimal;
+}
 int main(int argc, char const *argv[]){
-    const char *const words[] = {"a", "b", "c", "d"};
-    printf(".%s.",smash(words,4));
+        printf("%d",bin_to_decimal("001000"));   
     return 0;
 }
 
